3630fca7904fa37455a9bcaca723107082ca224c
SandroN <sandronardi87@gmail.com>
2024-06-19 17:57:06 +0200

    comments

--- Mass_FW_cancel_upgrade.py
+++ Mass_FW_cancel_upgrade.py
@@ -9,38 +9,50 @@ dashboard = meraki.DashboardAPI(API_KEY, suppress_logging=True)
 # get all arganization
 organizations = dashboard.organizations.getOrganizations()
 
+# for each org
 for org in organizations:
+    # get any scheduled upgrade
     upgrades = dashboard.organizations.getOrganizationFirmwareUpgrades(
         org["id"], total_pages="all", status="Scheduled"
     )
-    print(f"> Organization {org['name']}")
+    print(f"\n> Organization: {org['name']}")
+
+    # if no upgrade is scheduled go to next org
     if len(upgrades) == 0:
         print(f">> No upgrades scheduled for Organization {org['name']}")
         continue
 
+    # if not get all the tework in the org
     networks = dashboard.organizations.getOrganizationNetworks(
         org["id"], total_pages="all"
     )
 
-
+    # for each network get the scheduled upgrades
     for net in networks:
         net_updates = dashboard.networks.getNetworkFirmwareUpgrades(net["id"])
 
-        upgradeWindow = net_updates["upgradeWindow"]
-        timezone = net_updates["timezone"]
+        # dictionary with each pfoduct that has an upgrade that can be changed
         products_to_cancel = {}
+        # check if at least one can be cancelled
         more_than_zero_to_cancel = False
-        print(f">> Parsing: {net['name']}")
+
+        print(f"\n>> Parsing: {net['name']}")
+        # for all the product
         for k, product in net_updates["products"].items():
+
+            # if no upgrade is scheduled skip
             if product["nextUpgrade"]["time"] == "":
                 continue
 
-            more_than_zero_to_cancel = True
-
+            # if current version is not an available target version skip
             if product["currentVersion"] not in product["availableVersions"]:
-                print(f">>> Can't cancel {k}, destination version not available")
+                print(f">>> Can't cancel {k}, destination FW version not available")
                 continue
 
+            # else mark that there is at least 1 upgrade to cancel
+            more_than_zero_to_cancel = True
+
+            # add it in to the dictionary with the necessary configuration
             products_to_cancel[k] = {
                 "nextUpgrade": {
                     "time": product["nextUpgrade"]["time"],
@@ -49,16 +61,32 @@ for org in organizations:
                 "participateInNextBetaRelease": False,
             }
 
+            # console feedback
             print(
                 f">>> {k}, scheduled upgrade {product['nextUpgrade']['time']}, from {product['currentVersion']['shortName']} to from {product['nextUpgrade']['toVersion']['shortName']}"
             )
 
+        # if at least one to cancel
         if more_than_zero_to_cancel:
-            response = dashboard.networks.updateNetworkFirmwareUpgrades(
-                net["id"],
-                upgradeWindow=net_updates["upgradeWindow"],
-                timezone=net_updates["timezone"],
-                products=products_to_cancel,
-            )
+            # try to PUT
+            try:
+                response = dashboard.networks.updateNetworkFirmwareUpgrades(
+                    net["id"],
+                    upgradeWindow=net_updates["upgradeWindow"],
+                    timezone=net_updates["timezone"],
+                    products=products_to_cancel,
+                )
+                print(">>>> upgrade rescheduled")
+
+            except Exception as e:
+                print(">>>> unable to cancel")
+                print(e)
+
         else:
             print(f">>> Nothing to cancel for network {net['name']}")
+    # check if there are still scheduled upgrades
+    upgrades = dashboard.organizations.getOrganizationFirmwareUpgrades(
+        org["id"], total_pages="all", status="Scheduled"
+    )
+
+    print(f'\n>Total upgrades still scheduled for {org["name"]}: {len(upgrades)}')
